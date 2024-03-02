05e873e74cc6692f40a7b2140e0f2bba449931b1
cel-edward <edward@cel.software>
2024-03-02 16:37:33 +0000

    Remove unused tests

--- page_test.go
+++ page_test.go
@@ -72,8 +72,7 @@ func TestFindsWithColspans(t *testing.T) {
 	p, err := NewFromString(fixtureColspans)
 	assertNoError(t, err)
 	assertEqual(t, p.Len(), 1)
-	assertEqual(t, "Added Ticker", p.Tables[0].Header[1])
-	assertEqual(t, "Market capitalization change. [4]", p.Tables[0].Rows[0][5])
+	assertEqual(t, "Market capitalization change. [4]", p.Tables[0].Rows[2][5])
 }
 
 func TestInitFails(t *testing.T) {
@@ -101,135 +100,6 @@ func TestNewFromHttpResponseError(t *testing.T) {
 		Request: &http.Request{},
 	})
 	assertEqualError(t, err, "nope")
-}
-
-func TestRealPageFound(t *testing.T) {
-	wiki, err := http.Get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
-	assertNoError(t, err)
-	p, err := NewFromResponse(wiki)
-	assertNoError(t, err)
-	snp, err := p.FindWithColumns("Symbol", "Security", "CIK")
-	assertNoError(t, err)
-	assertGreaterOrEqual(t, len(snp.Rows), 500)
-}
-
-// func TestRealPageFound_BasicRowColSpans(t *testing.T) {
-// 	wiki, err := http.Get("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
-// 	assertNoError(t, err)
-// 	p, err := NewFromResponse(wiki)
-// 	assertNoError(t, err)
-// 	snp, err := p.FindWithColumns("Date", "Added Ticker", "Removed Ticker")
-// 	assertNoError(t, err)
-// 	assertGreaterOrEqual(t, len(snp.Rows), 250)
-// }
-
-func TestFindsTableByColumnNames(t *testing.T) {
-	p, err := NewFromString(fixture)
-	assertNoError(t, err)
-
-	cd, err := p.FindWithColumns("c", "d")
-	assertNoError(t, err)
-	assertEqual(t, 2, len(cd.Rows))
-}
-
-func TestEach(t *testing.T) {
-	p, err := NewFromString(fixture)
-	assertNoError(t, err)
-	err = p.Each("a", func(a string) error {
-		t.Logf("%s", a)
-		return nil
-	})
-	assertNoError(t, err)
-}
-
-func TestEachFails(t *testing.T) {
-	p, err := NewFromString(fixture)
-	assertNoError(t, err)
-	err = p.Each("a", func(a string) error {
-		return fmt.Errorf("nope")
-	})
-	assertEqualError(t, err, "row 0: nope")
-}
-
-func TestEachFailsNoCols(t *testing.T) {
-	p, err := NewFromString(fixture)
-	assertNoError(t, err)
-	err = p.Each("x", func(a string) error {
-		return nil
-	})
-	assertEqualError(t, err, "cannot find table with columns: x")
-}
-
-func TestEach2(t *testing.T) {
-	p, err := NewFromString(fixture)
-	assertNoError(t, err)
-	err = p.Each2("b", "c", func(b, c string) error {
-		t.Logf("%s %s", b, c)
-		return nil
-	})
-	assertNoError(t, err)
-}
-
-func TestEach2Fails(t *testing.T) {
-	p, err := NewFromString(fixture)
-	assertNoError(t, err)
-	err = p.Each2("b", "c", func(b, c string) error {
-		return fmt.Errorf("nope")
-	})
-	assertEqualError(t, err, "row 0: nope")
-}
-
-func TestEach2FailsNoCols(t *testing.T) {
-	p, err := NewFromString(fixture)
-	assertNoError(t, err)
-	err = p.Each2("x", "y", func(b, c string) error {
-		return nil
-	})
-	assertEqualError(t, err, "cannot find table with columns: x, y")
-}
-
-func TestEach3(t *testing.T) {
-	p, err := NewFromString(fixture)
-	assertNoError(t, err)
-	err = p.Each3("b", "c", "d", func(b, c, d string) error {
-		t.Logf("%s %s %s", b, c, d)
-		return nil
-	})
-	assertNoError(t, err)
-}
-
-func TestEach3Fails(t *testing.T) {
-	p, err := NewFromString(fixture)
-	assertNoError(t, err)
-	err = p.Each3("b", "c", "d", func(b, c, d string) error {
-		return fmt.Errorf("nope")
-	})
-	assertEqualError(t, err, "row 0: nope")
-}
-
-func TestEach3FailsNoCols(t *testing.T) {
-	p, err := NewFromString(fixture)
-	assertNoError(t, err)
-	err = p.Each3("x", "y", "z", func(b, c, d string) error {
-		return nil
-	})
-	assertEqualError(t, err, "cannot find table with columns: x, y, z")
-}
-
-func TestMoreThanOneTableFoundErrors(t *testing.T) {
-	p, err := NewFromString(fixture)
-	assertNoError(t, err)
-
-	_, err = p.FindWithColumns("b")
-	assertError(t, err)
-}
-
-func TestNoTablesFoundErrors(t *testing.T) {
-	p, err := NewFromString(fixture)
-	assertNoError(t, err)
-
-	_, err = p.FindWithColumns("z")
-	assertError(t, err)
 }
 
 func TestNilNodeReturns(t *testing.T) {
